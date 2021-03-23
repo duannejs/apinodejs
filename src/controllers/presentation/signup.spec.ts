d5a17a46fce0718ee8bb9a7eed428de80a8383c6
Duanne  Schaedler <duanneschaedler@MacBook-Air-de-Duanne.local>
2021-03-23 20:48:49 -0300

    Param

--- src/controllers/presentation/signup.spec.ts
+++ src/controllers/presentation/signup.spec.ts
@@ -1,8 +1,9 @@
+/* eslint-disable no-undef */
 import { SignUpController } from './signup'
 
 describe('SignUp Controller', () => {
   test('Should return 400 if no name is provided', async () => {
-    const  sut = new SignUpController()
+    const sut = new SignUpController()
     const httpRequest = {
       body: {
         email: 'any@email.com',
@@ -10,7 +11,8 @@ describe('SignUp Controller', () => {
         passwordConfirmation: 'any_password'
       }
     }
-    const httpResponse = await sut.handle(httpRequest)
+    const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param:name'))
   })
 })
