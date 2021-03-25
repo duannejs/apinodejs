e101dd1d453ad3c9ac76585c0bfb961cc3674db0
Duanne  Schaedler <duanneschaedler@MacBook-Air-de-Duanne.local>
2021-03-24 22:14:53 -0300

    sigUp

--- src/controllers/presentation/signup.ts
+++ src/controllers/presentation/signup.ts
@@ -1,18 +1,13 @@
 import { HttpRequest, HttpResponse } from '../presentation/protocols/http'
 import { MissingParamError } from './error/missing-param-error'
+import { badRequest } from '../presentation/helpres/http-helper'
 
 export class SignUpController {
   handle (httpRequest : HttpRequest) : HttpResponse {
-    if (!httpRequest.body.name) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('name')
-      }
-    }
-    if (!httpRequest.body.email) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('mail')
+    const requireFields = ['name', 'mail']
+    for (const field of requireFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field))
       }
     }
   }
