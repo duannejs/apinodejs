306961efbe008e1c52691455d3a9ab02d2735d61
Duanne  Schaedler <duanneschaedler@MacBook-Air-de-Duanne.local>
2021-03-24 22:02:53 -0300

    ajustes de erro

--- src/controllers/presentation/signup.spec.ts
+++ src/controllers/presentation/signup.spec.ts
@@ -1,5 +1,6 @@
 /* eslint-disable no-undef */
 import { SignUpController } from './signup'
+import { MissingParamError } from './error/missing-param-error'
 
 describe('SignUp Controller', () => {
   test('Should return 400 if no name is provided', async () => {
@@ -13,7 +14,7 @@ describe('SignUp Controller', () => {
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new Error('Missing param:name'))
+    expect(httpResponse.body).toEqual(new MissingParamError('name'))
   })
   test('Should return 400 if no email is provided', async () => {
     const sut = new SignUpController()
@@ -26,6 +27,6 @@ describe('SignUp Controller', () => {
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new Error('Missing param:mail'))
+    expect(httpResponse.body).toEqual(new MissingParamError('mail'))
   })
 })
