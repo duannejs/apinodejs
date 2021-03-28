66a3aa7e3c3b33fee873cec0365afbdbc0885896
Duanne  Schaedler <duanneschaedler@MacBook-Air-de-Duanne.local>
2021-03-28 08:42:18 -0300

    Novos requisitos email

--- src/controllers/presentation/signup.spec.ts
+++ src/controllers/presentation/signup.spec.ts
@@ -1,10 +1,31 @@
 /* eslint-disable no-undef */
 import { SignUpController } from './signup'
 import { MissingParamError } from './error/missing-param-error'
+import { InvalidParamError } from './error/invalid-param-error'
+import { EmailValidator } from './protocols/email-validator'
+
+interface SutTypes{
+  sut: SignUpController
+  emailValidatorStub: EmailValidator
+}
+
+const makeSut = (): SutTypes => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid (email: string): boolean {
+      return true
+    }
+  }
+  const emailValidatorStub = new EmailValidatorStub()
+  const sut = new SignUpController(emailValidatorStub)
+  return {
+    sut,
+    emailValidatorStub
+  }
+}
 
 describe('SignUp Controller', () => {
   test('Should return 400 if no name is provided', async () => {
-    const sut = new SignUpController()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         email: 'any@email.com',
@@ -18,7 +39,7 @@ describe('SignUp Controller', () => {
   })
 
   test('Should return 400 if no email is provided', async () => {
-    const sut = new SignUpController()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         name: 'any_name',
@@ -32,7 +53,7 @@ describe('SignUp Controller', () => {
   })
 
   test('Should return 400 if no password is provided', async () => {
-    const sut = new SignUpController()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         name: 'any_name',
@@ -45,7 +66,7 @@ describe('SignUp Controller', () => {
     expect(httpResponse.body).toEqual(new MissingParamError('password'))
   })
   test('Should return 400 if no confirmation is provided', async () => {
-    const sut = new SignUpController()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         name: 'any_name',
@@ -56,5 +77,20 @@ describe('SignUp Controller', () => {
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('passwordConfirmation'))
+  })
+  test('Should return 400 if invalid email is provided', async () => {
+    const { sut, emailValidatorStub } = makeSut()
+    jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        mail: 'invalid_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new InvalidParamError('mail'))
   })
 })
--- src/controllers/presentation/signup.ts
+++ src/controllers/presentation/signup.ts
@@ -2,14 +2,25 @@ import { HttpRequest, HttpResponse } from '../presentation/protocols/http'
 import { MissingParamError } from './error/missing-param-error'
 import { badRequest } from '../presentation/helpres/http-helper'
 import { Controller } from '../presentation/protocols/controller'
+import { EmailValidator } from './protocols/email-validator'
+import { InvalidParamError } from './error/invalid-param-error'
 
 export class SignUpController implements Controller {
+  private readonly emailValidator: EmailValidator
+  constructor (emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator
+  }
+
   handle (httpRequest : HttpRequest) : HttpResponse {
     const requireFields = ['name', 'mail', 'password', 'passwordConfirmation']
     for (const field of requireFields) {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field))
       }
+    }
+    const isValid = this.emailValidator.isValid(httpRequest.body.mail)
+    if (!isValid) {
+      return badRequest(new InvalidParamError('mail'))
     }
   }
 }
